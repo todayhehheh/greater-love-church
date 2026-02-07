89aa0a3dff2253e6d19731f7421d8ffbc3d83a3f
todayhehheh <todayhehheh@gmail.com>
2026-02-07 19:26:13 +0900

    Updates

--- _next/static/2nCPwxBU6QGcCzdo19m9j/_buildManifest.js
+++ _next/static/2nCPwxBU6QGcCzdo19m9j/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/greater-love-church//_next/:path+",
+        "destination": "/greater-love-church/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
